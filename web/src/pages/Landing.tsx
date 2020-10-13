c4897210bbbe994a97f295b079da75ec0a96db5d
Daniel Oliveira <daniel.go@msn.com>
2020-10-13 01:05:11 -0300

    Configurando link para usar o react-router-dom

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import { FiArrowRight } from 'react-icons/fi';
 
 import '../styles/pages/landing.css';
@@ -17,9 +18,9 @@ const Landing = () => {
                     <span>Minas Gerais</span>
                 </div>
 
-                <a href="" className="enter-app">
+                <Link to="/app" className="enter-app">
                     <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-                </a>
+                </Link>
             </div>
         </div>
     )
