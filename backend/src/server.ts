e3f6c4e67ce25e919bc27b115e30d46d0513680f
Daniel Oliveira <daniel.go@msn.com>
2020-10-14 18:09:40 -0300

    Adicionando e configurando o pacote cors no backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import './database/connection'
 import 'express-async-errors'
 
+import cors from 'cors'
 import express from 'express'
 import path from 'path'
 
@@ -9,6 +10,7 @@ import routes from './routes'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(errorHandler)
