9e6278c7aab42cb3cff68db84358cdf67c2e63f7
Daniel Oliveira <daniel.go@msn.com>
2020-10-14 00:05:50 -0300

    Configurando tratamento de erros do backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,16 @@
 import './database/connection'
+import 'express-async-errors'
 
 import express from 'express'
 import path from 'path'
 
+import errorHandler from './errors/handler'
 import routes from './routes'
 
 const app = express()
 app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler)
 
 app.listen(3333)
