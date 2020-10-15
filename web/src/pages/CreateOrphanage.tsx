f847423ef45af93dc985f76eeefe9175a50b1bfe
Daniel Oliveira <daniel.go@msn.com>
2020-10-14 22:29:36 -0300

    Componentização da sidebar e  mapIcon

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,107 +1,85 @@
-import React from "react";
+import React from 'react';
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
-import { useHistory } from "react-router-dom";
-
-import { FiArrowLeft, FiPlus } from "react-icons/fi";
-
-import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/create-orphanage.css';
+import { FiPlus } from 'react-icons/fi';
+import Sidebar from '../components/Sidebar';
 
-const happyMapIcon = L.icon({
-  iconUrl: mapMarkerImg,
+import mapIcon from '../utils/mapIcon';
 
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-})
+export default function CreateOrphanage(): JSX.Element {
+    return (
+        <div id="page-create-orphanage">
+            <Sidebar />
 
-export default function CreateOrphanage() {
-  const { goBack } = useHistory();
+            <main>
+                <form className="create-orphanage-form">
+                    <fieldset>
+                        <legend>Dados</legend>
 
-  return (
-    <div id="page-create-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
+                        <Map center={[-27.2092052, -49.6401092]} style={{ width: '100%', height: 280 }} zoom={15}>
+                            <TileLayer
+                                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                            />
 
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+                            <Marker interactive={false} icon={mapIcon} position={[-27.2092052, -49.6401092]} />
+                        </Map>
 
-      <main>
-        <form className="create-orphanage-form">
-          <fieldset>
-            <legend>Dados</legend>
+                        <div className="input-block">
+                            <label htmlFor="name">Nome</label>
+                            <input id="name" />
+                        </div>
 
-            <Map 
-              center={[-27.2092052,-49.6401092]} 
-              style={{ width: '100%', height: 280 }}
-              zoom={15}
-            >
-              <TileLayer 
-                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-              />
+                        <div className="input-block">
+                            <label htmlFor="about">
+                                Sobre <span>Máximo de 300 caracteres</span>
+                            </label>
+                            <textarea id="name" maxLength={300} />
+                        </div>
 
-              <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
-            </Map>
+                        <div className="input-block">
+                            <label htmlFor="images">Fotos</label>
 
-            <div className="input-block">
-              <label htmlFor="name">Nome</label>
-              <input id="name" />
-            </div>
+                            <div className="uploaded-image"></div>
 
-            <div className="input-block">
-              <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
-              <textarea id="name" maxLength={300} />
-            </div>
+                            <button className="new-image">
+                                <FiPlus size={24} color="#15b6d6" />
+                            </button>
+                        </div>
+                    </fieldset>
 
-            <div className="input-block">
-              <label htmlFor="images">Fotos</label>
+                    <fieldset>
+                        <legend>Visitação</legend>
 
-              <div className="uploaded-image">
+                        <div className="input-block">
+                            <label htmlFor="instructions">Instruções</label>
+                            <textarea id="instructions" />
+                        </div>
 
-              </div>
+                        <div className="input-block">
+                            <label htmlFor="opening_hours">Nome</label>
+                            <input id="opening_hours" />
+                        </div>
 
-              <button className="new-image">
-                <FiPlus size={24} color="#15b6d6" />
-              </button>
-            </div>
-          </fieldset>
+                        <div className="input-block">
+                            <label htmlFor="open_on_weekends">Atende fim de semana</label>
 
-          <fieldset>
-            <legend>Visitação</legend>
+                            <div className="button-select">
+                                <button type="button" className="active">
+                                    Sim
+                                </button>
+                                <button type="button">Não</button>
+                            </div>
+                        </div>
+                    </fieldset>
 
-            <div className="input-block">
-              <label htmlFor="instructions">Instruções</label>
-              <textarea id="instructions" />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="opening_hours">Nome</label>
-              <input id="opening_hours" />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="open_on_weekends">Atende fim de semana</label>
-
-              <div className="button-select">
-                <button type="button" className="active">Sim</button>
-                <button type="button">Não</button>
-              </div>
-            </div>
-          </fieldset>
-
-          <button className="confirm-button" type="submit">
-            Confirmar
-          </button>
-        </form>
-      </main>
-    </div>
-  );
+                    <button className="confirm-button" type="submit">
+                        Confirmar
+                    </button>
+                </form>
+            </main>
+        </div>
+    );
 }
 
 // return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
