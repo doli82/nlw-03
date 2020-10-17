1e2d063789b323a6c1475626951cefc65e220077
Daniel Oliveira <daniel.go@msn.com>
2020-10-17 00:56:30 -0300

    Criando o fluxo de navegação do cadastro de orfanatos

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,19 +1,53 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React from "react";
+import React from 'react';
 
-import OrphanageDetails from "./pages/OrphanageDatails";
+import OrphanageDetails from './pages/OrphanageDatails';
 import OrphanagesMap from './pages/OrphanagesMap';
 
-const {Navigator, Screen} = createStackNavigator();
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/Header';
 
-export default function Routes() {
-    return (
-        <NavigationContainer>
-            <Navigator screenOptions={{headerShown: false}}>
-                <Screen name="OrphanagesMap" component={OrphanagesMap} />
-                <Screen name="OrphanageDetails" component={OrphanageDetails} />
-            </Navigator>
-        </NavigationContainer>
-    );
+const { Navigator, Screen } = createStackNavigator();
+
+export default function Routes(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: '#f2f3f5' }
+        }}
+      >
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+        <Screen
+          name="OrphanageDetails"
+          component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: (): JSX.Element => (
+              <Header title="Orfanato" showCancel={false} />
+            )
+          }}
+        />
+        <Screen
+          name="SelectMapPosition"
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: (): JSX.Element => <Header title="Selecione a Posição" />
+          }}
+        />
+        <Screen
+          name="OrphanageData"
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: (): JSX.Element => <Header title="Informe os Dados" />
+          }}
+        />
+      </Navigator>
+    </NavigationContainer>
+  );
 }
